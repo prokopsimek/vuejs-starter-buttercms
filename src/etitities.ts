e3ce9a481bee2f41b5c921237298e854b846d29f
prokopsimek <prokop@dxheroes.io>
2022-03-22 08:48:33 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/etitities.ts
@@ -0,0 +1,17 @@
+export type NavigationItem = {
+  meta: { id: number };
+  url: string;
+  label: string;
+};
+
+export type FeatureType = {
+  headline: string;
+  description: string;
+  icon: string;
+};
+
+export type TestimonialType = {
+  quote: string;
+  name: string;
+  title: string;
+};
